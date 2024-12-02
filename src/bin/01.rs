a0b6d97672385e183ac9c58c9379f0db95365569
Ivan Peng <ivanpeng13@gmail.com>
2024-12-02 00:50:42 -0500

    part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,12 +1,14 @@
 use anyhow::*;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::iter::{zip};
+use std::iter::{zip, Map, Zip};
 use std::num;
+use std::ops::Mul;
 use std::path::absolute;
 use std::vec::IntoIter;
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
+use itertools::Itertools;
 use adv_code_2024::*;
 
 const DAY: &str = "01";
@@ -24,7 +26,7 @@ const TEST: &str = "\
 fn main() -> Result<()> {
     start_day(DAY);
 
-    fn split_to_two_sorted_lists<R: BufRead>(reader: R) -> Zip<IntoIter<i32>, IntoIter<i32>> {
+    fn split_to_two_sorted_lists<R: BufRead>(reader: R) -> (Vec<i32>, Vec<i32>) {
         let mut l1: Vec<i32> = Vec::new();
         let mut l2: Vec<i32> = Vec::new();
         for line in reader.lines() {
@@ -35,18 +37,19 @@ fn main() -> Result<()> {
         }
         l1.sort();
         l2.sort();
-        zip(l1, l2)
+        (l1, l2)
     }
     //region Part 1
     println!("=== Part 1 ===");
 
     fn part1<R: BufRead>(reader: R) -> Result<usize> {
-        let zipped_list = split_to_two_sorted_lists(reader);
+        let (l1, l2) = split_to_two_sorted_lists(reader);
+        let zipped_list = zip(l1, l2);
         let mut acc: u32 = 0;
         for zip_value in zipped_list {
             acc += zip_value.0.abs_diff(zip_value.1)
         }
-        Ok(usize::try_from(acc)?)
+        Ok(acc as usize)
     }
 
     // TODO: Set the expected answer for the test input
@@ -58,17 +61,24 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        let (l1, l2) = split_to_two_sorted_lists(reader);
+        let m2 = l2.iter().counts();
+        let mut acc: usize = 0;
+        for elem in l1 {
+            let v = elem as usize;
+            acc += v.mul(m2.get(&elem).unwrap_or(&0));
+        }
+        Ok(acc)
+    }
+
+    assert_eq!(31, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
