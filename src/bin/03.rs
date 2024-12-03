4c5e7d548947585b4ffe0acbde5a5f726d389d99
Ivan Peng <ivanpeng13@gmail.com>
2024-12-03 01:44:54 -0500

    day 3 pt 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,7 +1,7 @@
 use anyhow::*;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::ops::Mul;
+use std::ops::{Add, Mul};
 use std::str::FromStr;
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
@@ -12,7 +12,7 @@ const DAY: &str = "03";
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
 
 const TEST: &str = "\
-xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
 fn main() -> Result<()> {
     start_day(DAY);
@@ -31,7 +31,6 @@ fn main() -> Result<()> {
                 acc += x.mul(y) as usize;
             }
         }
-
         Ok(acc)
     }
 
@@ -43,17 +42,42 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(mut reader: R) -> Result<usize> {
+        let mut acc: usize = 0;
+        let key: Vec<&str> =vec!["do()", "don't()"];
+        let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)")?;
+        let mut contents = reader.lines().collect::<Result<Vec<_>, _>>()?.join("\n");
+        let mut substr = contents;
+        let mut should_extract: bool = true;
+        while substr.len() > 0 {
+            let start: usize = 0;
+            let end = substr.find(key[should_extract as usize]).unwrap_or(substr.len());
+
+            let sequence = &substr[start..end];
+            // dbg!("substr: {}, sequence: {}, should_extract: {}", &substr, &sequence, should_extract);
+            if should_extract {
+                for captures in re.captures_iter(sequence) {
+                    let x = i32::from_str(captures.get(1).unwrap().as_str())?;
+                    let y = i32::from_str(captures.get(2).unwrap().as_str())?;
+                    acc += x.mul(y) as usize;
+                }
+            }
+            // shrink substr and flip the switch
+            let start = if end.add(key[should_extract as usize].len()) >= substr.len() { substr.len() } else { end.add(key[should_extract as usize].len()) };
+            substr = substr[start..substr.len()].parse()?;
+            should_extract = !should_extract;
+
+        }
+        Ok(acc)
+    }
+
+    assert_eq!(48, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
